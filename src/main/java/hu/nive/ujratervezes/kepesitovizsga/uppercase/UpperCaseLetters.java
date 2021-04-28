7b830b9d483ffa528bd86530ac5a46674362890f
n0rb1v <n0rb1v@gmail.com>
2021-04-28 20:56:43 +0200

    write

--- src/main/java/hu/nive/ujratervezes/kepesitovizsga/uppercase/UpperCaseLetters.java
+++ src/main/java/hu/nive/ujratervezes/kepesitovizsga/uppercase/UpperCaseLetters.java
@@ -1,6 +1,7 @@
 package hu.nive.ujratervezes.kepesitovizsga.uppercase;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -17,9 +18,18 @@ public class UpperCaseLetters {
                     }
                 }
             }
+            writeFile("" + sum);
             return sum;
         } catch (IOException e) {
             throw new IllegalStateException("file error");
+        }
+    }
+
+    private void writeFile(String s) {
+        try (BufferedWriter bw = Files.newBufferedWriter(Path.of("src/main/resources/out.txt"))) {
+            bw.write(s + "\n");
+        } catch (IOException e) {
+            throw new IllegalStateException("Can't write file", e);
         }
     }
 }
